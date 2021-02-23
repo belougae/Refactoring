c6d6576012947aa1a8609a2eac98ae9abfaefb56
Belouga.E <wangjingnic1993@gmail.com>
2021-02-23 20:05:27 +0800

    .

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   var plays = {
-    "hamlet": {"name": "Hamlet", "type": "tragedy"},
-    "as-like": {"name": "As You Like It", "type": "comedy"},
-    "othello": {"name": "Othello", "type": "tragedy"}
+    hamlet: { name: 'Hamlet', type: 'tragedy' },
+    'as-like': { name: 'As You Like It', type: 'comedy' },
+    othello: { name: 'Othello', type: 'tragedy' }
   }
 
   var invoice = {
@@ -26,19 +26,23 @@ $(document).ready(function () {
   statement(invoice, plays)
 })
 
-
-function volumeCreditsFor(aPerformance, plays) {
+function volumeCreditsFor (aPerformance, plays) {
   let result = 0
   // add volume credits
   result += Math.max(aPerformance.audience - 30, 0)
 
   // add extra credit for every ten comedy attendees
-  if ('comedy' === playFor(aPerformance, plays).type) result += Math.floor(aPerformance.audience / 5)
-  return result;
+  if ('comedy' === playFor(aPerformance, plays).type)
+    result += Math.floor(aPerformance.audience / 5)
+  return result
 }
 
-function usd(aNumber) {
-  return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber);
+function usd (aNumber) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+  }).format(aNumber)
 }
 
 function statement (invoice, plays) {
@@ -47,12 +51,12 @@ function statement (invoice, plays) {
   let result = `Statement for ${invoice.customer}`
 
   for (let performance of invoice.performances) {
-    volumeCredits += volumeCreditsFor(performance, plays);
+    volumeCredits += volumeCreditsFor(performance, plays)
 
     // print line for this order
-    result += ` ${playFor(performance, plays).name}: ${usd(amountFor(performance, plays) / 100)}(${
-        performance.audience
-    } seats)\n`
+    result += ` ${playFor(performance, plays).name}: ${usd(
+      amountFor(performance, plays) / 100
+    )}(${performance.audience} seats)\n`
     totalAmount += amountFor(performance, plays)
   }
   result += `Amount owed is ${usd(totalAmount / 100)}\n`
@@ -61,7 +65,7 @@ function statement (invoice, plays) {
   alert(result)
 }
 
-function amountFor(aPerformance, plays) {
+function amountFor (aPerformance, plays) {
   let result = 0
   switch (playFor(aPerformance, plays).type) {
     case 'tragedy':
@@ -80,11 +84,9 @@ function amountFor(aPerformance, plays) {
     default:
       throw new Error(`unknown type: ${playFor(aPerformance, plays).type}`)
   }
-  return result;
+  return result
 }
 
-function playFor(aPerformance, plays) {
-  return plays[aPerformance.playID];
+function playFor (aPerformance, plays) {
+  return plays[aPerformance.playID]
 }
-
-
