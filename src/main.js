aae434df705f8439054cca0a6dc459a26ac92e18
Belouga.E <wangjingnic1993@gmail.com>
2021-02-23 20:04:37 +0800

    改变函数名

--- src/main.js
+++ src/main.js
@@ -27,32 +27,35 @@ $(document).ready(function () {
 })
 
 
+function volumeCreditsFor(aPerformance, plays) {
+  let result = 0
+  // add volume credits
+  result += Math.max(aPerformance.audience - 30, 0)
+
+  // add extra credit for every ten comedy attendees
+  if ('comedy' === playFor(aPerformance, plays).type) result += Math.floor(aPerformance.audience / 5)
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber);
+}
+
 function statement (invoice, plays) {
   let totalAmount = 0
   let volumeCredits = 0
   let result = `Statement for ${invoice.customer}`
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2
-  }).format;
 
   for (let performance of invoice.performances) {
-    let thisAmount = amountFor(performance, plays);
-
-    // add volume credits
-    volumeCredits += Math.max(performance.audience - 30, 0)
-
-    // add extra credit for every ten comedy attendees
-    if ('comedy' === playFor(performance, plays).type) volumeCredits += Math.floor(performance.audience / 5)
+    volumeCredits += volumeCreditsFor(performance, plays);
 
     // print line for this order
-    result += ` ${playFor(performance, plays).name}: ${format(thisAmount / 100)}(${
+    result += ` ${playFor(performance, plays).name}: ${usd(amountFor(performance, plays) / 100)}(${
         performance.audience
     } seats)\n`
-    totalAmount += thisAmount
+    totalAmount += amountFor(performance, plays)
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`
+  result += `Amount owed is ${usd(totalAmount / 100)}\n`
   result += `You earned ${volumeCredits} credits\n`
 
   alert(result)
