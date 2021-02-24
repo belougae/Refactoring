440612bb9292d31e1b95facf1c422175207afb54
Belouga.E <wangjingnic1993@gmail.com>
2021-02-24 11:01:19 +0800

    1.替换renderPlainText中对playFor的所有引用点 2.使用类似的手法搬移amountFor函数

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ function volumeCreditsFor(aPerformance) {
   result += Math.max(aPerformance.audience - 30, 0)
 
   // add extra credit for every ten comedy attendees
-  if ('comedy' === playFor(aPerformance).type)
+  if ('comedy' === aPerformance.play.type)
     result += Math.floor(aPerformance.audience / 5)
   return result
 }
@@ -25,28 +25,6 @@ function usd (aNumber) {
     currency: 'USD',
     minimumFractionDigits: 2
   }).format(aNumber)
-}
-
-function amountFor(aPerformance) {
-  let result = 0
-  switch (aPerformance.play.type) {
-    case 'tragedy':
-      result = 40000
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30)
-      }
-      break
-    case 'comedy':
-      result = 30000
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20)
-      }
-      result += 300 * aPerformance.audience
-      break
-    default:
-      throw new Error(`unknown type: ${aPerformance.play.type}`)
-  }
-  return result
 }
 
 
@@ -62,15 +40,15 @@ function totalVolumeCredits(data){
 function  totalAmount(data) {
   let result = 0
   for (let performance of data.performances) {
-    result += amountFor(performance)
+    result += performance.play.amount
   }
   return result;
 }
 
 function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
-  for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+  for (let performance of data.performances) {
+    result += ` ${performance.play.name}: ${usd(performance.play.amount)} (${performance.audience} seats)\n`;
   }
   result += `Amount owed is ${usd(totalAmount(data))}\n`;
   result += `You earned ${totalVolumeCredits(data)} credits\n`;
@@ -105,6 +83,7 @@ function statement () {
 function enrichPerformance(aPerformance) {
   const result = Object.assign({}, aPerformance);
   result.play = playFor(result);
+  result.amount = amountFor(result);
   return result;
 }
 
@@ -112,3 +91,24 @@ function playFor(aPerformance) {
   return plays[aPerformance.playID];
 }
 
+function amountFor(aPerformance) {
+  let result = 0
+  switch (aPerformance.play.type) {
+    case 'tragedy':
+      result = 40000
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30)
+      }
+      break
+    case 'comedy':
+      result = 30000
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20)
+      }
+      result += 300 * aPerformance.audience
+      break
+    default:
+      throw new Error(`unknown type: ${aPerformance.play.type}`)
+  }
+  return result
+}
